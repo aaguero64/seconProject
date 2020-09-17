d46cada76f9762ede2ea0b0ac5113bc37bcbe436
Adrian Aguero <aaguero64@hotmail.com>
2020-09-16 17:28:42 -0700

    secon project

---
+++ customers.java
@@ -0,0 +1,21 @@
+package seconProject;
+
+public class customers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Albert";
+		String lastName = "Brown";
+		String title = "admin";
+		String address = "123 First St";
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(title);
+		System.out.println(address);
+		
+				
+				
+	}
+
+}
